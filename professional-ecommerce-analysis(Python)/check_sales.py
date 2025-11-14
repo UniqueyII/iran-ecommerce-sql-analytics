24f84dc78f5d8c88147d58cfde1412f0f4efdfab
Amin Sharifi <112435808+UniqueyII@users.noreply.github.com>
2025-11-14 21:53:04 +0330

    Delete professional-ecommerce-analysis(Python) directory

--- professional-ecommerce-analysis(Python)/check_sales.py
+++
@@ -1,9 +0,0 @@
-# check_sales.py
-import os
-
-analysis_folder = 'src/analysis'
-if os.path.exists(analysis_folder):
-    files = os.listdir(analysis_folder)
-    print(f"📁 Files in {analysis_folder}: {files}")
-else:
-    print(f"❌ Folder {analysis_folder} not found")
